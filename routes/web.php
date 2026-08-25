4766768feebdc98b1269a891df510f47ca257251
Sania <saniakhan94392@gmail.com>
2026-08-26 03:44:23 +0530

    Add root route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('courses.index');
+});
+
 Route::resource('courses', CourseController::class);
